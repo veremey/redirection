5bbb57d27a1c30c8f78d50384c5e683b3dda6909
veremey <veremey4uk@gmail.com>
2024-05-28 16:20:26 +0200

    env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import dotenv from "dotenv"
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	define: {
+		// env variable
+		"process.env.VITE_SUPABASE_URL": JSON.stringify(process.env.VITE_SUPABASE_URL),
+		"process.env.VITE_SUPABASE_ANON_KEY": JSON.stringify(
+			process.env.VITE_SUPABASE_ANON_KEY
+		),
+	},
 })
